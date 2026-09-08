30bc8a5122d9b22b84bbcd1b5e0d8ec910e74e7a
Arad Fadaei <aradfadaie@gmail.com>
2026-09-08 00:14:37 -0400

    fix: update valid.jpg and polygonal blur example

--- examples/polygon_blur.rs
+++ examples/polygon_blur.rs
@@ -24,13 +24,18 @@ fn run(input: OsString, output: OsString) -> ditherlib::Result<()> {
     let width = source.width() as f32;
     let height = source.height() as f32;
     let polygon = Polygon::new([
-        Point::new(width * 0.20, height * 0.25),
-        Point::new(width * 0.75, height * 0.15),
-        Point::new(width * 0.85, height * 0.70),
-        Point::new(width * 0.30, height * 0.85),
+        Point::new(width * 0.50, height * 0.10),
+        Point::new(width * 0.59, height * 0.38),
+        Point::new(width * 0.88, height * 0.38),
+        Point::new(width * 0.65, height * 0.55),
+        Point::new(width * 0.74, height * 0.84),
+        Point::new(width * 0.50, height * 0.67),
+        Point::new(width * 0.26, height * 0.84),
+        Point::new(width * 0.35, height * 0.55),
+        Point::new(width * 0.12, height * 0.38),
+        Point::new(width * 0.41, height * 0.38),
     ])?;
-    let rendered =
-        Renderer::new().render(&source, &Blur::new(50.0)?, &Selection::Polygon(polygon))?;
+    let rendered = Renderer::new().render(&source, &Blur::new(32.0)?, &Selection::Polygon(polygon))?;
 
     write(output, &rendered)
 }
