accbabd74ad66a8804e41e5a00b37d0380920fad
Arad Fadaei <aradfadaie@gmail.com>
2026-09-17 17:03:01 -0400

    docs: blur after dithering in pipeline example

--- examples/pipeline.rs
+++ examples/pipeline.rs
@@ -23,12 +23,12 @@ fn run(input: OsString, output_directory: PathBuf) -> common::Result {
     let source = read(input)?;
     let mut pipeline = Pipeline::new();
     pipeline.add(Greyscale, Selection::All);
-    pipeline.add(Blur::new(1.5)?, Selection::All);
     pipeline.add(
         OrderedDither::new(Palette::black_and_white(), ThresholdMap::bayer_4x4())
             .with_pixel_size(4, 4)?,
         Selection::All,
     );
+    pipeline.add(Blur::new(1.5)?, Selection::All);
 
     let rendered = Renderer::new().render_pipeline(&source, &pipeline)?;
     write(output_directory.join("pipeline.png"), &rendered)?;
